a6810101a89f83176f7a903709ebcb88d56a457f
Won Soyeon <wonsy1006@gmail.com>
2022-04-27 21:18:19 +0900

    Add files via upload

---
+++ Chapter3_8.java
@@ -0,0 +1,20 @@
+package thisIsJava;
+
+public class Chapter3_8 {
+
+	public static void main(String[] args) {
+		double x = 5.0;
+		double y = 0.0;
+		
+		double z = x % y;
+		
+		if(Double.isNaN(z)) { // Double은 Class, isNaN은 Method
+			System.out.println("0.0으로 나눌 수 없습니다.");
+		} else {
+			double result = z + 10;
+			System.out.println("결과 : " + result);
+		}
+
+	}
+
+}
