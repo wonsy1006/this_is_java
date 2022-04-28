8aa43a448cc5680e2f3d16499df9be43fea1bd0d
Won Soyeon <wonsy1006@gmail.com>
2022-04-28 19:27:34 +0900

    Add files via upload

--- Chapter4_7.java
+++ Chapter4_7.java
@@ -8,15 +8,17 @@ public class Chapter4_7 {
 		boolean run = true;
 		
 		int balance = 0;
-		int choice;
-		int deposit;
-		int withdraw;
+
 		
 		while(run) {
 			System.out.println("--------------------------------------");
 			System.out.println("1. 예금 | 2. 출금 | 3. 잔고 | 4. 종료");
 			System.out.println("--------------------------------------");
 			System.out.print("선택 > ");
+			
+			int choice;
+			int deposit;
+			int withdraw;
 			
 			Scanner sc = new Scanner(System.in);
 			choice = sc.nextInt();
@@ -30,22 +32,31 @@ public class Chapter4_7 {
 					
 					balance += deposit;
 					
+					System.out.println();
+					
 				} else if(choice == 2) {
-					System.out.print("출금액 > ");
+					System.out.print("출금액 > " );
 					
 					Scanner  withdrawSc = new Scanner(System.in);
 					withdraw = withdrawSc.nextInt();
 					
 					balance -= withdraw;
 					
+					if(balance < 0) {
+						System.out.println("잔액이 부족하여 출금할 수 없습니다.");
+					}
+					
+					System.out.println();
+					
 				} else if(choice == 3) {
 					
 					System.out.print("잔고 > " + balance + "\n");
+					System.out.println();
 				
 				} else break;
 				
 			} else {
-				System.out.println("1부터 4까지의 숫자만 입력해주세요"); 
+				System.out.println("1부터 4까지의 숫자만 입력해주세요.\n"); 
 			}
 			
 		}
