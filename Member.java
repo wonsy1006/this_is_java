1069262dfff398813a017c4713461e4ab1276a78
Won Soyeon <wonsy1006@gmail.com>
2022-05-03 21:32:13 +0900

    Add files via upload

---
+++ Member.java
@@ -0,0 +1,15 @@
+package thisIsJava;
+
+public class Member {
+	// 필드
+	String name;
+	String id;
+	String password;
+	int age;
+	
+	// 생성자
+	public Member(String name, String id) {
+		this.name = name;
+		this.id = id;
+	}
+}
